225260f977e6221cc85b30017b0d50a4906dab89
Bowen Revill <bowenrevill4@gmail.com>
2022-09-20 11:39:33 -0600

    first commit

---
+++ scripts/pallet.js
@@ -0,0 +1,140 @@
+var palletId = location.search.substring(1)
+
+$(document).ready(function() {
+    populatePage()
+    populateFormDropdowns()
+    $('#itemEditAlert').hide()
+    $('#itemEditSuccess').hide()
+
+    $('#back-to-orders').click(function() {
+        window.location.href = `index.html`
+    })
+
+    $('#submitItemChanges').click(function() {
+        let errors = validateForm()
+        if (errors == '') {
+            editItem($('#idNumber').html())
+            $('#itemEditSuccess').show()
+
+        } else {
+            $('#itemEditAlert').html(errors)
+            $('#itemEditAlert').show()
+        }
+    })
+
+    $('#cancelEdit').click(function() {
+        $('#itemEditAlert').hide()  
+        $('#itemEditSuccess').hide()       
+    })
+})
+
+const editItem = async (itemId) => {
+    const request = await fetch(`http://localhost:8080/items/${itemId}`, {
+        method: "PUT",
+        Headers: {
+            Accept: 'application.json',
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({
+            "itemName": `${$('#editItemName').val()}`,
+            "itemDescription": `${$('#editItemDescription').val()}`,
+            "itemPrice": `${$('#editItemPrice').val()}`,
+            "palletId": `${$('#palletIdNumber').html()}`,
+            "sold": `${$('#editItemSold').val()}`,
+            "category": `${$('#editCategory').val()}`,
+            "sellDate": `${$('#editSellDate').val()}`,
+            "sellPrice": `${$('#editSellPrice').val()}`,
+            "platform": `${$('#editPlatform').val()}`
+        }),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    const data = await request.json()
+    console.log(data)
+    return data
+}
+
+function validateForm() {
+    let errors = ''
+    if ($('#editItemName').val() == '') {
+        errors += 'Item name cannot be empty.\n'
+    }
+    if ($('#editItemPrice').val() <= 0) {
+        errors += 'Retail price must be greater than 0.'
+    }
+    if ($('editSellPrice') < 0) {
+        errors += 'Sell price must be greater than 0.'
+    }
+    return errors
+}
+
+const getItem = async (itemId) => {
+    const request = await fetch(`http://localhost:8080/items/${itemId}`)
+    const data = await request.json()
+    return data
+}
+
+const itemsInPallet = async (palletId) => {
+    const request = await fetch(`http://localhost:8080/items/pallet/${palletId}`)
+    const data = await request.json()
+    return data
+}
+
+const getPallet = async (palletId) => {
+    const request = await fetch(`http://localhost:8080/pallets/${palletId}`)
+    const data = await request.json()
+    return data
+}
+
+async function populatePage() {
+    let palletInfo = await getPallet(palletId)
+    let items = await itemsInPallet(palletId)
+    $('#palletTitle').html(palletInfo.palletName)
+    for (let item of items) {
+        $('#itemList').append(`
+            <div class="row border p-3 item-row ${item.sold ? 'item-row-bought' : ''}">
+                <div class="col-2">${item.itemName}</div>
+                <div class="col-3">${item.itemDescription}</div>
+                <div class="col-1">${item.itemPrice}</div>
+                <div class="col-1">${item.sellPrice && item.sellPrice != 0 ? item.sellPrice : '-'}</div>
+                <div class="col-2">${item.sellDate != null && item.sellDate != '1899-11-30T07:00:00.000Z' ? item.sellDate : '-'}</div>
+                <div class="col-2">${item.platform != null ? item.platform : '-'}</div>
+                <div class="col-1"><i class=" editItemBtn fa-regular fa-pen-to-square edit-item" data-bs-toggle="modal" data-bs-target="#myModal" data-itemId="${item.itemId}"></i></div>
+            </div>
+        `)
+    }
+    $('.editItemBtn').click(function() {
+        populateForm($(this).attr('data-itemId'))
+    })
+}
+
+async function populateFormDropdowns() { 
+    let categoriesInfo = await getCategoriesDropdown()
+    let $catDD = $('#editCategory')
+    for (let category of categoriesInfo) {
+        $catDD.append(`
+            <option value="${category.categoryId}">${category.categoryName}</option>
+        `)
+    }
+}
+
+const getCategoriesDropdown = async () => {
+    const request = await fetch(`http://localhost:8080/categories/dropdown`)
+    const data = await request.json()
+    return data
+}
+
+async function populateForm(itemId) {
+    let itemData = await getItem(itemId)
+    let item = itemData[0]
+    $('#editItemName').val(item.itemName)
+    $('#editItemDescription').val(item.itemDescription)
+    $('#editItemPrice').val(item.itemPrice)
+    $('#editItemSold').val(item.sold)
+    $('#editCategory').val(item.category)
+    $('#editSellDate').val(item.sellDate)
+    $('#editSellPrice').val(item.sellPrice)
+    $('#editPlatform').val(item.platform)
+    $('#idNumber').html(`${item.itemId}`)
+    $('#palletIdNumber').html(`${item.palletId}`)
+} 
+
---
+++ scripts/script.js
@@ -0,0 +1,285 @@
+$(document).ready(function() {
+    populateDashboard()
+    populateDropdowns()
+    populateCategories()
+    $('#itemAlert').hide()
+    $('#itemSuccess').hide()
+    $('#categoryAlert').hide()
+    $('#categorySuccess').hide()
+    $('#orderAlert').hide()
+    $('#orderSuccess').hide()
+    
+    $('#submitNewItem').click(function() {
+        $('#itemAlert').hide()
+        $('#itemSuccess').hide()
+        let validation = itemValidation()
+        if (validation == '') {
+            for (let i = 0; i < $('#itemQty').val(); i++) {
+                newItem()
+            }
+            console.log('called newitem()')
+            clearItemInputs()
+            $('#itemSuccess').show()
+
+        } else {
+            $('#itemAlert').show()
+            $('#itemAlert').html(
+                `<p>${validation}</p>`
+            )
+        }
+    })
+
+    $('#addCategory').click(function() {
+        $('#categoryAlert').hide()
+        $('#categorySuccess').hide()
+        let validation = categoryValidation()
+        if (validation == false) {
+            $('#categoryAlert').show()
+        } else {
+            $('#categorySuccess').show()
+            newCategory()
+            $('#newCategory').val('')
+
+        }
+    })
+
+    $('#submitNewOrder').click(function() {
+        $('#orderAlert').hide()
+        $('#orderSuccess').hide()
+        let validation = orderValidation()
+        if (validation == '') {
+            console.log('called newOrder()')
+            newOrder()
+            clearOrderInputs()
+            $('#orderSuccess').show()
+            
+        } else {
+            $('#orderAlert').show()
+            $('#orderAlert').html(
+                `<p>${validation}</p>`
+            )
+        }
+    })
+
+})
+
+function clearOrderInputs() {
+    $('#orderName').val('')
+    $('#orderDate').val('')
+    $('#orderPrice').val('')
+}
+
+function clearItemInputs() {
+    $('#itemName').val('')
+    $('#itemQty').val('')
+    $('#retailPrice').val('')
+}
+
+function itemValidation() {
+    let errorText = ''
+    if ($('#itemName').val() == '') {
+        errorText += 'Item name is required.\n'
+    } 
+    if ($('#itemQty').val() < 1) {
+        errorText += 'Item quantity must be at least one.\n'
+    } 
+    if ($('#retailPrice').val() <= 0) {
+        errorText += 'Item price must be greater than 0.'
+    }
+    return errorText
+}
+
+function orderValidation() {
+    let errorText = ''
+    if ($('#orderName').val() == '') {
+        errorText += 'Order name is required.\n'
+    }
+    if ($('#orderDate').val() == '') {
+        errorText += 'Order date is required.\n'
+    }
+    if ($('#orderPrice').val() <= 0) {
+        errorText += 'Order price must be greater than 0.'
+    }
+    return errorText
+}
+
+const newItem = async () => {
+    const request = await fetch(`http://localhost:8080/items`, {
+        
+        method: "POST",
+        Headers: {
+            Accept: 'application.json',
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({
+            "itemName": `${$('#itemName').val()}`,
+            "itemDescription": `temporary`,
+            "palletId": `${$('#palletNum').val()}`,
+            "itemPrice": `${$('#retailPrice').val()}`,
+            "sold": false,
+            "category": `${$('#itemCategory').val()}`,
+            "sellDate": null,
+            "sellPrice": null,
+            "platform": 'temporary'
+        }),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    const data = await request.json()
+    console.log(data)
+    return data
+}
+
+const newOrder = async () => {
+    const request = await fetch(`http://localhost:8080/pallets`, {
+        method: "POST",
+        headers: {
+            Accept: 'application.json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "palletName": `${$('#orderName').val()}`,
+            "purchaseDate": `${$('#orderDate').val()}`,
+            "purchasePrice": `${Number($('#orderPrice').val())}`
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    const data = await request.json()
+    console.log(data)
+    return data
+}
+
+const activePallets = async () => {
+    const request = await fetch('http://localhost:8080/pallets')
+    const data = await request.json()
+    return data
+}
+
+const itemsInPallet = async (palletId) => {
+    const request = await fetch(`http://localhost:8080/items/pallet/${palletId}`)
+    const data = await request.json()
+    return data
+}
+
+const populateDashboard = async () => {
+    let totalProfit = 0
+    let $palletsContainer = $('#palletsContainer')
+    let pallets = await activePallets()
+    for (let pallet of pallets) {
+        let items = await itemsInPallet(pallet.palletId)
+        let palletProfit = Math.round((totalSoldItems(items) - Number(pallet.purchasePrice)) * 100) / 100
+        $palletsContainer.append(
+            `<div class="border row my-3 pallet-row" data-pid="${pallet.palletId}">
+                <div class="col-3">${pallet.palletName}</div>
+                <div class="col-2 d-flex justify-content-center">$${pallet.purchasePrice}</div>
+                <div class="col-2 d-flex justify-content-center">${(pallet.purchaseDate)}</div>
+                <div class="col-2 d-flex justify-content-center">${countSoldItems(items)}/${items.length} items sold</div>
+                <div class="col-3 d-flex justify-content-center ${palletProfit > 0 ? 'profit-green': 'profit-red'}">$${palletProfit}
+            </div>`
+        )
+        totalProfit += Math.round((totalSoldItems(items) - pallet.purchasePrice) * 100)/100
+    }
+
+    $('.pallet-row').hover(function() {
+        $(this).css('background-color', 'lightblue')                                               
+    },
+    function() {
+        $(this).css('background-color', 'white')
+    })
+
+    $('.pallet-row').click(function() {
+        console.log(this)
+        window.location.href = `pallet.html?${$(this).attr('data-pid')}`
+    })
+
+    $('#profitNumber').html(totalProfit)
+    $('#profitSpan').css('color', ((totalProfit > 0) ? 'green' : 'red'))
+}
+
+const populateCategories = async () => {
+    let categories = await getCategoriesDropdown()
+    let $catList = $('#categoryList')
+    for (let category of categories) {
+        $catList.append(
+            `<li>${category.categoryName}</li>`
+        )
+    }
+}
+
+const getPalletsDropdown = async () => {
+    const request = await fetch('http://localhost:8080/pallets/dropdown')
+    const data = await request.json()
+    return data
+}
+
+const populateDropdowns = async () => {
+    let palletsInfo = await getPalletsDropdown()
+    let categoriesInfo = await getCategoriesDropdown()
+    let $catDD = $('#itemCategory')
+    let $palletDD = $('#palletNum')
+    for (let category of categoriesInfo) {
+        $catDD.append(
+            `<option value="${category.categoryId}">${category.categoryName}</option>`
+        )
+    }
+    for (let pallet of palletsInfo) {
+        $palletDD.append(
+            `<option value="${pallet.palletId}">${pallet.palletName}</option>`
+        )
+    }
+    }
+
+const getCategoriesDropdown = async () => {
+    const request = await fetch(`http://localhost:8080/categories/dropdown`)
+    const data = await request.json()
+    return data
+}
+
+
+function countSoldItems(items) {
+    let count = 0
+    for (let item of items) {
+        if (item.sold == true) {
+            count ++
+        }
+    }
+    return count
+}
+
+function totalSoldItems(items) {
+    let total = 0
+    for (let item of items) {
+        if (item.sold == true) {
+            total += Number(item.sellPrice)
+        }
+    }
+    return total
+}
+
+const newCategory = async () => {
+    const request = await fetch(`http://localhost:8080/categories`, {
+        
+        method: "POST",
+        Headers: {
+            Accept: 'application.json',
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({
+            "categoryName": `${$('#newCategory').val()}`
+        }),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    const data = await request.json()
+    console.log(data)
+    return data
+}
+
+function categoryValidation() {
+    if ($('#newCategory').val() == '') {
+        return false
+    }
+    return true
+}
+
+
